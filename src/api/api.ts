7f268b180e959ce1b1b909c0cdcd0f1fb4031bde
Nikolay Krishtopa <nikolay.krishtopa@gmail.com>
2023-05-03 00:42:55 +0400

    refactor: mograte previously missed api to TS

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
+import { ProfileType } from '../models/models'
 
 const BASE_URL = 'https://social-network.samuraijs.com/api/1.0/'
 const API_KEY = '06fa1e34-2eda-4911-ba6d-106750cf7c2d'
@@ -19,17 +20,17 @@ const usersApi = {
     )
   },
 
-  followUser(id) {
+  followUser(id: number) {
     return axiosReq.post(`follow/${id}`)
   },
 
-  unfollowUser(id) {
+  unfollowUser(id: number) {
     return axiosReq.delete(`follow/${id}`)
   },
 }
 
 const authApi = {
-  login(values) {
+  login(values: {login: string, email:string}) {
     return axiosReq.post(BASE_URL + 'auth/login', values)
   },
   logout() {
@@ -41,16 +42,16 @@ const authApi = {
 }
 
 const profileApi = {
-  getUserData(userId) {
+  getUserData(userId: number) {
     return axiosReq.get(BASE_URL + 'profile/' + userId)
   },
-  getUserStatus(userId) {
+  getUserStatus(userId: number) {
     return axiosReq.get(BASE_URL + 'profile/status/' + userId)
   },
-  updateMyStatus(statusText) {
+  updateMyStatus(statusText: string) {
     return axiosReq.put(BASE_URL + 'profile/status', { status: statusText })
   },
-  updateMyProfile(profileData) {
+  updateMyProfile(profileData: AxiosRequestConfig<{}>) {
     return axiosReq.put(BASE_URL + 'profile', {}, profileData)
   },
 }
